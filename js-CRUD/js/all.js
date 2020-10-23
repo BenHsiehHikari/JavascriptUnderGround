c40f7fa6e1d0d2435a2014816f0a6bfc1f2a3ec4
謝政宏 <b3194359@gmail.com>
2020-10-23 15:55:07 +0800

    更新 crud的刪除功能

--- js-CRUD/js/all.js
+++ js-CRUD/js/all.js
@@ -1,93 +1,78 @@
 const productURL = 'http://localhost:1337/products/';
-let ablumid = document.querySelector('#ablumid');
-let ablumtitle = document.querySelector('#ablumtitle');
-let photourl = document.querySelector('#photourl');
+let productid = document.querySelector('#product-id');
+let productname = document.querySelector('#product-name');
+let productseller = document.querySelector('#product-seller');
+let productprice = document.querySelector('#product-price');
+
 let btnCreate = document.querySelector('#btn-create');
 let btnRead = document.querySelector('#btn-read');
 let btnUpdate = document.querySelector('#btn-update');
 let btnDeleteAll = document.querySelector('#btn-delete');
 
+const tbody = document.querySelector('#tbody');
+let output = '';
+
+const renderProducts = (products) => {
+  products.map(function(item, index, array){
+    output += `
+      <tr data-id=${item.id}>
+        <th class="id" scope="row">${index+1}</th>
+        <td class="name">${item.productName}</td>
+        <td class="seller">${item.productSeller}</td>
+        <td class="price">${item.productPrice}</td>
+        <td class="edit"><i id="edit-product" class="fas fa-edit btnedit"></i></td>
+        <td class="delete"><i id="delete-product" class="fas fa-trash-alt btndelete"></i></td>
+      </tr>
+    `;
+  });
+  tbody.innerHTML = output;
+}
+
 function createNode(element) {
   return document.createElement(element); 
 }
-
 function append(parent, el) {
   return parent.appendChild(el);
 }
 
 async function fetchData(){
-  const response = await fetch( productURL ,{
+  const responseprdouct = await fetch( productURL ,{
     method: 'GET',
     headers: {
       'Content-type': 'application/json; charset=UTF-8',
     },
   });
-  const productList = await response.json();
-  console.log(productList);
-  const tbody = document.querySelector('#tbody');
-  productList.map(function(item){
-      let tr = createNode('tr');
-      let th = createNode('th');
-      let td1 = createNode('td');
-      let td2 = createNode('td');
-      let td3 = createNode('td');
-      let td4 = createNode('td');
-      let td5 = createNode('td');
-      let img = createNode('img');
-      let iEdit = createNode('i');
-      let iDel = createNode('i');
-  
-      th.textContent = item.id;
+  const productList = await responseprdouct.json();
 
-      td1.classList.add("name");
-      td1.textContent = item.productName;
-  
-      td2.classList.add("seller");
-      td2.textContent = item.productSeller;
-
-      td3.classList.add("price");
-      td3.textContent = item.productPrice;
-
-      td4.classList.add("edit");
-      iEdit.className = "fas fa-edit btnedit";
-
-      td5.classList.add("delete");
-      iDel.className = "fas fa-trash-alt btndelete";
-      
-      append(tr, th);
-
-      append(tr, td1);
-  
-      append(tr, td2);
-  
-      append(tr, td3);
-  
-      append(td4, iEdit);
-      append(tr, td4);
-
-      append(td5, iDel);
-      append(tr, td5);
-  
-      append(tbody, tr);
-    })
-}
-
-
-async function createAlbum(){
-  const response = await fetch( productURL , {
-    method: 'POST',
+  const responsecount = await fetch( productURL+'count' ,{
+    method: 'GET',
     headers: {
       'Content-type': 'application/json; charset=UTF-8',
     },
-    body:JSON.stringify({
-      productName: 'iphone 12',
-      productSeller: '船長',
-      productPrice: 30000,
-    }), 
   });
-  const data = await response.json();
-  console.log(data);
+  const count = await responsecount.json();
+
+  return productList;
 }
+fetchData().then(function(data){
+  const fakeData = [...data];
+  // console.log(fakeData);
+  renderProducts(fakeData);
+})
+
+// async function createproduct(){
+//     const response = await fetch( productURL , {
+//       method: 'POST',
+//       headers: {
+//         'Content-type': 'application/json; charset=UTF-8',
+//       },
+//       body:JSON.stringify({
+//         productName: 'lol',
+//         productSeller: 'ben',
+//         productPrice : 100
+//       }), 
+//     });
+// }
 
 async function updateData(){
   const url = 'https://jsonplaceholder.typicode.com/photos/1';
@@ -108,23 +93,50 @@ async function updateData(){
   console.log(data);
 }
 
-async function deleteData(){
-  const url = 'https://jsonplaceholder.typicode.com/photos/1';
+async function deleteProduct(){
+  const url = `http://localhost:1337/products/`;
   const response = await fetch(url, {
     method: 'DELETE',
   })
   const data = await response.json();
   console.log(data);
+  location.reload();
 }
-
-
-
-
-
-window.addEventListener('load', function(){
-  fetchData();
-})
+deleteProduct();
 
 btnCreate.addEventListener('click', function(){
-  
+  fetch(productURL, {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+    body:JSON.stringify({
+      productName: productname.value,
+      productSeller: productseller.value,
+      productPrice : productprice.value
+    })
+  })
+  .then(res => res.json())
+  .then(data => {
+        const dataArr = [];
+        dataArr.push(data);
+        renderProducts(dataArr);
+  })
+
+  productname.value = "";
+  productseller.value = "";
+  productprice.value = "";
 });
+
+
+tbody.addEventListener('click' , function(e){
+  e.preventDefault();
+  let delButtonPressed = e.target.id == 'delete-product';
+  let editButtonPressed = e.target.id == 'edit-product';
+
+  let id = e.target.parentElement.parentElement.dataset.id;
+
+  if(delButtonPressed){
+    deleteProduct(id);
+  }
+})
